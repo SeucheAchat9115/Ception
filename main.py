710c9b25ed3d27bb74794d77d0b78f4a9abb81dd
Maximilian Menke <65967380+SeucheAchat9115@users.noreply.github.com>
2024-12-20 20:09:27 +0000

    Added config

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import argparse
 
+def main(args):
+    pass
 
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="DeepFrame is a deep learning framework"
+        )
+    parser.add_argument(
+        "--config", 
+        type=str, 
+        default="DeepFrame/configs/config.yaml", 
+        help="Config to overwrite default hyperparameters"
+        )
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    pass
+    args = parse_args()
+    main(args)
