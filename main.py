6e56e231b815f30dab217d41d62f7f24a832e559
Maximilian Menke <maximilianmenke@web.de>
2024-12-23 19:43:38 +0000

    Fixing pre-commits

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+"""Main Function of the framework"""
+
 import argparse
 
 
-def main(args):
-    pass
+def main(args: argparse.Namespace) -> None:
+    """
+    Main entry point into the framework
+
+    Args:
+        args (argparse.Namespace): Arguments collected by argparser
+    """
+    assert isinstance(args.config, str)
+    print(args)
 
 
 def parse_args() -> argparse.Namespace:
     """
     Parse command line arguments
+
+    Returns:
+        argparse.Namespace: Arguments passed on the cli interface
     """
+
     parser = argparse.ArgumentParser(
         description="DeepFrame is a deep learning framework"
     )
@@ -22,5 +35,4 @@ def parse_args() -> argparse.Namespace:
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main(parse_args())
