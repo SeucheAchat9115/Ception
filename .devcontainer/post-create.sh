4722b459e2ca7567b9fad0d92ca11ae0392b3ff2
Maximilian Menke <65967380+SeucheAchat9115@users.noreply.github.com>
2025-01-11 09:01:18 +0000

    Adding devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Creating virtual environment and installing dependencies"
+python -m venv .venv
+source .venv/bin/activate
+echo "Venv created under .venv"
+echo "Installing project using poetry"
+poetry install
