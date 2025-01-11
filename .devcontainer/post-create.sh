7af1bc5ffb4c1feafa4f3f064e90424032b69696
Maximilian Menke <65967380+SeucheAchat9115@users.noreply.github.com>
2025-01-11 09:18:35 +0000

    Rename venv

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Creating virtual environment and installing dependencies"
-python -m venv .venv
-source .venv/bin/activate
-echo "Venv created under .venv"
+python -m venv cp-venv
+source cp-venv/bin/activate
+echo "Venv created under cp-venv"
 echo "Installing project using poetry"
 poetry install
 pre-commit install
